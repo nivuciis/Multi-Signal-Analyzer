83c962202349d4c86c672264c6cbe5b5881ca44d
{nivuciis} <vinicius.rafael751@gmail.com>
2025-11-24 16:14:41 -0300

    chore: added tinyUSB to project and set channel command

---
+++ Multi-Signal-Analyzer/tusb_config.h
@@ -0,0 +1,42 @@
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// --------------------------------------------------------------------+
+// CONTROLLER CONFIGURATION
+// --------------------------------------------------------------------+
+#define CFG_TUSB_MCU                OPT_MCU_RP2040 // RP2350 uses the same USB IP as RP2040
+
+// --------------------------------------------------------------------+
+// DEVICE CONFIGURATION
+// --------------------------------------------------------------------+
+#define CFG_TUD_ENABLED             1
+
+#define CFG_TUSB_RHPORT0_MODE \
+    (OPT_MODE_DEVICE | OPT_MODE_FULL_SPEED)
+#ifndef BOARD_TUD_RHPORT 
+#define BOARD_TUD_RHPORT          0
+#endif
+
+// Enable the Vendor Class 
+#define CFG_TUD_VENDOR              1
+
+#define CFG_TUD_CDC              1
+
+// Buffer sizes (Bulk transfers are usually 64 bytes on USB FS)
+#define CFG_TUD_CDC_RX_BUFSIZE   64
+#define CFG_TUD_CDC_TX_BUFSIZE   64
+#define CFG_TUD_CDC_EP_BUFSIZE   64
+
+#ifndef CFG_TUD_ENDPOINT0_SIZE
+#define CFG_TUD_ENDPOINT0_SIZE    64
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _TUSB_CONFIG_H_ */

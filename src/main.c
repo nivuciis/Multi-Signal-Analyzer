c0af19184ed717d4542680733a421719f6da8bce
João Dias <joao.dias@edge.ufal.br>
2026-01-26 14:53:14 -0300

    fix: Ajusta chamada do teste

--- src/main.c
+++ src/main.c
@@ -10,7 +10,16 @@
  *
  *******************************************************************/
 
+#include "adc.h"
+#include "can.h"
+#include "cmd.h"
+#include "gpios.h"
 #include "led.h"
+#include "rs232.h"
+#include "rs485.h"
+
+#include <stdbool.h>
+#include <stdio.h>
 
 #include <pico/stdio_usb.h>
 #include <pico/time.h>
@@ -19,18 +28,59 @@ static bool is_usb_connected = false;
 
 static void sync_led_with_usb_connection()
 {
-    is_usb_connected = stdio_usb_connected();
-    ana_led_set_status((is_usb_connected) ? LED_STATUS_CONNECTED : LED_STATUS_OFF);
+	is_usb_connected = stdio_usb_connected();
+	ana_led_set_status((is_usb_connected) ? LED_STATUS_CONNECTED : LED_STATUS_OFF);
+}
+
+static enum ana_cmd_code get_cmd_by_serial(void)
+{
+	int cmd = getchar_timeout_us(1000);
+	if (cmd != PICO_ERROR_TIMEOUT) {
+		return cmd;
+	}
+	return ANA_CMD_NONE;
+}
+
+static void _cmd_table()
+{
+	printf("\n \tAvailable Commands:\n");
+	printf("0x00 - Reserved\n");
+	printf("0x01 - ADC Test\n");
+	printf("0x02 - CAN Test\n");
+	printf("0x03 - GPIOS Test\n");
+	printf("0x04 - LED Test\n");
+	printf("0x05 - RS232 Test\n");
+	printf("0x06 - RS485 Test\n\n");
 }
 
 int main()
 {
+	enum ana_cmd_code cmd;
+
 	stdio_init_all();
+
+	sleep_ms(500);
+
+	ana_adc_init();
+	ana_adc_set_clkdiv(10.0f);
+	ana_can_init();
+	ana_gpios_init();
 	ana_led_init();
+	ana_rs232_init();
+	ana_rs485_init();
+
+	_cmd_table();
 
 	while (1) {
 		sync_led_with_usb_connection();
-        sleep_ms(30);
+
+		if (is_usb_connected) {
+			cmd = get_cmd_by_serial();
+			if (cmd != 0) {
+				ana_cmd_process(cmd);
+			}
+		}
+		sleep_ms(30);
 	}
 
 	return 0;

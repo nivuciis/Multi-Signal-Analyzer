b67d901c4e300ab3fb3812fdc0d64560ce7dfc02
João Dias <joao.dias@edge.ufal.br>
2026-01-26 14:51:20 -0300

    refactor: Generalização do modulos de teste

--- includes/config_pio.h
+++ includes/config_pio.h
@@ -11,25 +11,52 @@
 #include <pico/time.h>
 #include <pico/types.h>
 
+#define ANA_CONFIG_MODULE_NAME_MAX_LEN 8
+
 /**
- * @brief Configuration structure for PIO programs and DMA
+ * @brief Configuration structure for each module
+ *
+ */
+struct ana_config_modulo {
+	char name[ANA_CONFIG_MODULE_NAME_MAX_LEN]; /**< Name of the module */
+	uint pin_base;                                    /**< Base GPIO pin number */
+	uint pin_count;                                   /**< Number of GPIO pins */
+	uint samples;                                     /**< Number of samples */
+};
+
+/**
+ * @brief Configuration structure for DMA
+ *
+ */
+struct ana_config_dma {
+	uint instance;                   /**< DMA channel number */
+	dma_channel_config instance_cfg; /**< DMA channel configuration */
+	void (*callback)(void);      /**< DMA callback function */
+	uint16_t *dma_buffer;            /**< DMA buffer pointer */
+	volatile bool has_complete;      /**< DMA completion flag */
+};
+
+/**
+ * @brief Configuration structure for PIO
  *
  */
 struct ana_config_pio {
-	PIO pio;                          /**< PIO instance */
-	uint sm;                          /**< State machine index */
+	PIO instance;                          /**< PIO instance */
+	uint sm;                          /**< State machine number */
 	const pio_program_t *pio_program; /**< Pointer to the PIO program */
-	uint pio_offset; /**< Offset of the PIO program in the PIO instruction memory */
+	uint pio_offset;                  /**< Offset of the PIO program */
 	pio_sm_config (*get_default_cfg_func)(
-		uint offset);             /**< Function to get default SM config */
-	uint pin_base;                    /**< Base GPIO pin number */
-	uint pin_count;                   /**< Number of GPIO pins */
-	uint samples;                     /**< Number of samples to capture */
-	uint dma_chan0;                   /**< DMA channel 0 */
-	dma_channel_config dma_chan0_cfg; /**< DMA channel 0 configuration */
-	void (*dma_callback)(void);       /**< DMA completion callback function */
-	uint16_t *dma_buffer;             /**< DMA buffer */
-	volatile bool dma_complete;       /**< DMA completion flag */
+		uint offset); /**< Function to get default state machine configuration */
+};
+
+/**
+ * @brief Configuration structure for PIO programs and DMA modele
+ *
+ */
+struct ana_config_system {
+	struct ana_config_modulo module; /**< Module configuration */
+	struct ana_config_pio pio;       /**< PIO configuration */
+	struct ana_config_dma dma;       /**< DMA configuration */
 };
 
 /**
@@ -37,43 +64,77 @@ struct ana_config_pio {
  *
  * @param config Structure referencing the PIO program and DMA configuration
  */
-void ana_config_pio_init(struct ana_config_pio *config);
+void ana_config_pio_init(struct ana_config_system *config);
 
 /**
  * @brief Initialize the DMA configuration for the PIO program
  *
- * @param config Structure referencing the PIO program and DMA configuration
+ * @param config Structure referencing the module configuration
  */
-void ana_config_pio_dma_init(struct ana_config_pio *config);
+void ana_config_pio_dma_init(struct ana_config_system *config);
 
 /**
  * @brief Abort the DMA operation for the PIO program
  *
- * @param config Structure referencing the PIO program and DMA configuration
+ * @param config Structure referencing the module configuration
  */
-void ana_config_pio_dma_abort(struct ana_config_pio *config);
+void ana_config_pio_dma_abort(struct ana_config_system *config);
 
 /**
  * @brief Start the DMA operation for the PIO program
  *
- * @param config Structure referencing the PIO program and DMA configuration
+ * @param config Structure referencing the module configuration
  */
-void ana_config_pio_dma_start(struct ana_config_pio *config);
+void ana_config_pio_dma_start(struct ana_config_system *config);
 
 /**
  * @brief Wait for the DMA operation to complete for the PIO program
  *
- * @param config Structure referencing the PIO program and DMA configuration
+ * @param config Structure referencing the module configuration
  */
-void ana_config_pio_dma_wait(struct ana_config_pio *config);
+void ana_config_pio_dma_wait(struct ana_config_system *config);
 
 /**
  * @brief Check if the DMA operation is busy for the PIO program
  *
- * @param config Structure referencing the PIO program and DMA configuration
+ * @param config Structure referencing the module configuration
  * @return true  If the DMA is busy
  * @return false Otherwise
  */
-bool ana_config_pio_dma_is_busy(struct ana_config_pio *config);
+bool ana_config_pio_dma_is_busy(struct ana_config_system *config);
+
+/**
+ * @brief Get data from PIO program with DMA.
+ *
+ * @param config Structure referencing the module configuration
+ */
+void ana_config_pio_get_data(struct ana_config_system *config);
+
+/**
+ * @brief Get data from PIO program with DMA (version 2).
+ *
+ */
+void ana_config_pio_get_data_v2(struct ana_config_system *config);
+
+/**
+ * @brief Print PIO program data.
+ *
+ * @param config Structure referencing the module configuration
+ */
+void ana_config_pio_print_data(struct ana_config_system *config);
+
+/**
+ * @brief Test PIO direct functionality.
+ *
+ * @param config Structure referencing the module configuration
+ */
+void ana_config_pio_test_pio_direct(struct ana_config_system *config);
+
+/**
+ * @brief Get buffer of PIO program data.
+ *
+ * @param config Structure referencing the module configuration
+ */
+uint16_t *ana_config_pio_get_buffer(struct ana_config_system *config);
 
 #endif
--- src/config_pio.c
+++ src/config_pio.c
@@ -13,98 +13,245 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <hardware/clocks.h>
 #include <hardware/dma.h>
+#include <hardware/gpio.h>
 #include <hardware/irq.h>
 #include <hardware/pio.h>
 #include <pico/error.h>
 
-void ana_config_pio_init(struct ana_config_pio *config)
+void ana_config_pio_init(struct ana_config_system *config)
 {
-	config->pio = pio0;
-	config->sm = pio_claim_unused_sm(config->pio, true);
+    printf("DEBUG: Initializing PIO for module %s\n", config->module.name);
 
-	config->dma_complete = false;
+    int pin;
 
-	pio_sm_set_enabled(config->pio, config->sm, false);
+	config->pio.instance = pio0;
+	config->pio.sm = pio_claim_unused_sm(config->pio.instance, true);
 
-	config->pio_offset = pio_add_program(config->pio, config->pio_program);
-	pio_sm_config sm_cfg = config->get_default_cfg_func(config->pio_offset);
+	config->dma.has_complete = false;
 
-	for (int i = 0; i < config->pin_count; i++) {
-		pio_gpio_init(config->pio, config->pin_base + i);
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+	config->pio.pio_offset = pio_add_program(config->pio.instance, config->pio.pio_program);
+	pio_sm_config sm_cfg = config->pio.get_default_cfg_func(config->pio.pio_offset);
+
+	for (int i = 0; i < config->module.pin_count; i++) {
+		pin = config->module.pin_base + i;
+		pio_gpio_init(config->pio.instance, pin);
+		gpio_pull_down(pin);
 	}
 
-	pio_sm_set_consecutive_pindirs(config->pio, config->sm, config->pin_base, config->pin_count,
-				       false);
+	pio_sm_set_consecutive_pindirs(config->pio.instance, config->pio.sm,
+				       config->module.pin_base, config->module.pin_count, false);
 
-	sm_config_set_wrap(&sm_cfg, config->pio_offset,
-			   config->pio_offset + config->pio_program->length - 1);
-	sm_config_set_in_pins(&sm_cfg, config->pin_base);
-	sm_config_set_in_shift(&sm_cfg, false, true, config->pin_count);
+	sm_config_set_wrap(&sm_cfg, config->pio.pio_offset,
+			   config->pio.pio_offset + config->pio.pio_program->length - 1);
+	sm_config_set_in_pins(&sm_cfg, config->module.pin_base);
+	sm_config_set_in_shift(&sm_cfg, false, true, config->module.pin_count);
 
-	float clkdiv = clock_get_hz(clk_sys) / 1000000.0f;
+	float clkdiv = clock_get_hz(clk_sys) / 10000.0f;
 	sm_config_set_clkdiv(&sm_cfg, clkdiv);
 
 	sm_config_set_fifo_join(&sm_cfg, PIO_FIFO_JOIN_RX);
 
-	pio_sm_init(config->pio, config->sm, config->pio_offset, &sm_cfg);
-	pio_sm_clear_fifos(config->pio, config->sm);
-	pio_sm_set_enabled(config->pio, config->sm, false);
+	pio_sm_init(config->pio.instance, config->pio.sm, config->pio.pio_offset, &sm_cfg);
+	pio_sm_clear_fifos(config->pio.instance, config->pio.sm);
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
 }
 
-void ana_config_pio_dma_init(struct ana_config_pio *config)
+void ana_config_pio_dma_init(struct ana_config_system *config)
 {
-	config->dma_chan0 = dma_claim_unused_channel(true);
+    printf("DEBUG: Initializing DMA for module %s\n", config->module.name);
 
-	config->dma_chan0_cfg = dma_channel_get_default_config(config->dma_chan0);
-	channel_config_set_read_increment(&config->dma_chan0_cfg, false);
-	channel_config_set_write_increment(&config->dma_chan0_cfg, true);
-	channel_config_set_transfer_data_size(&config->dma_chan0_cfg, DMA_SIZE_16);
-	channel_config_set_dreq(&config->dma_chan0_cfg,
-				pio_get_dreq(config->pio, config->sm, false));
+	config->dma.instance = dma_claim_unused_channel(true);
 
-	dma_channel_configure(config->dma_chan0, &config->dma_chan0_cfg, config->dma_buffer,
-			      &config->pio->rxf[config->sm], config->samples, false);
+	config->dma.instance_cfg = dma_channel_get_default_config(config->dma.instance);
+	channel_config_set_read_increment(&config->dma.instance_cfg, false);
+	channel_config_set_write_increment(&config->dma.instance_cfg, true);
+	channel_config_set_transfer_data_size(&config->dma.instance_cfg, DMA_SIZE_16);
+	channel_config_set_dreq(&config->dma.instance_cfg,
+				pio_get_dreq(config->pio.instance, config->pio.sm, false));
+
+	dma_channel_configure(config->dma.instance, &config->dma.instance_cfg,
+			      config->dma.dma_buffer, &config->pio.instance->rxf[config->pio.sm],
+			      config->module.samples, false);
 }
 
-void ana_config_pio_dma_start(struct ana_config_pio *config)
+void ana_config_pio_dma_start(struct ana_config_system *config)
 {
-	if (config->dma_callback) {
-		irq_set_exclusive_handler(DMA_IRQ_0, config->dma_callback);
+	if (config->dma.callback) {
+		irq_set_exclusive_handler(DMA_IRQ_0, config->dma.callback);
 		irq_set_enabled(DMA_IRQ_0, true);
-		dma_channel_set_irq0_enabled(config->dma_chan0, true);
+		dma_channel_set_irq0_enabled(config->dma.instance, true);
 	}
 
-	config->dma_complete = false;
+	config->dma.has_complete = false;
 
-	dma_channel_configure(config->dma_chan0, &config->dma_chan0_cfg, config->dma_buffer,
-			      &config->pio->rxf[config->sm], config->samples, false);
+	dma_channel_configure(config->dma.instance, &config->dma.instance_cfg,
+			      config->dma.dma_buffer, &config->pio.instance->rxf[config->pio.sm],
+			      config->module.samples, false);
 
-	dma_channel_start(config->dma_chan0);
+	dma_channel_start(config->dma.instance);
 }
 
-void ana_config_pio_dma_wait(struct ana_config_pio *config)
+void ana_config_pio_dma_wait(struct ana_config_system *config)
 {
-	dma_channel_wait_for_finish_blocking(config->dma_chan0);
-	config->dma_complete = true;
+	dma_channel_wait_for_finish_blocking(config->dma.instance);
+	config->dma.has_complete = true;
 }
 
-bool ana_config_pio_dma_is_busy(struct ana_config_pio *config)
+bool ana_config_pio_dma_is_busy(struct ana_config_system *config)
 {
-	return dma_channel_is_busy(config->dma_chan0);
+	return dma_channel_is_busy(config->dma.instance);
 }
 
-void ana_config_pio_dma_abort(struct ana_config_pio *config)
+void ana_config_pio_dma_abort(struct ana_config_system *config)
 {
-	dma_channel_abort(config->dma_chan0);
+	dma_channel_abort(config->dma.instance);
 
-	if (config->dma_callback) {
-		dma_channel_set_irq0_enabled(config->dma_chan0, false);
+	if (config->dma.callback) {
+		dma_channel_set_irq0_enabled(config->dma.instance, false);
 		irq_set_enabled(DMA_IRQ_0, false);
-		irq_remove_handler(DMA_IRQ_0, config->dma_callback);
+		irq_remove_handler(DMA_IRQ_0, config->dma.callback);
 	}
 
-	config->dma_complete = true;
+	config->dma.has_complete = true;
+}
+
+void ana_config_pio_get_data(struct ana_config_system *config)
+{
+	printf("Start GPIO capture\n");
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+
+	memset(config->dma.dma_buffer, 0, sizeof(uint16_t) * config->module.samples);
+
+	pio_sm_clear_fifos(config->pio.instance, config->pio.sm);
+	pio_sm_restart(config->pio.instance, config->pio.sm);
+
+	dma_channel_configure(config->dma.instance, &config->dma.instance_cfg,
+			      config->dma.dma_buffer, &config->pio.instance->rxf[config->pio.sm],
+			      config->module.samples, false);
+	dma_channel_start(config->dma.instance);
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, true);
+	dma_channel_wait_for_finish_blocking(config->dma.instance);
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+	printf("DMA transfer finished\n");
+
+	if (dma_channel_is_busy(config->dma.instance)) {
+		printf("ERROR: DMA still busy! Aborting...\n");
+		dma_channel_abort(config->dma.instance);
+	} else {
+		printf("DMA completed successfully\n");
+	}
+}
+
+void ana_config_pio_get_data_v2(struct ana_config_system *config)
+{
+	printf("Start GPIO capture (v2)\n");
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+	memset(config->dma.dma_buffer, 0, sizeof(uint16_t) * config->module.samples);
+	pio_sm_clear_fifos(config->pio.instance, config->pio.sm);
+	pio_sm_restart(config->pio.instance, config->pio.sm);
+
+	ana_config_pio_dma_start(config);
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, true);
+	ana_config_pio_dma_wait(config);
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+	printf("DMA transfer finished (v2)\n");
+}
+
+void ana_config_pio_print_data(struct ana_config_system *config)
+{
+	if (config->module.pin_count == 0 || config->module.pin_count > 16) {
+		printf("ERROR: Invalid pin_count: %u\n", config->module.pin_count);
+		return;
+	}
+
+	printf("GPIO Data (each column is a pin, left-to-right = pin_map[0]..pin_map[n-1]):\n");
+
+	printf("Pins:   ");
+	for (uint i = 0; i < config->module.pin_count; i++) {
+		printf("%3u", config->module.pin_base + i);
+	}
+	printf("\n");
+
+	printf("Sample: ");
+	for (uint i = 0; i < config->module.pin_count; i++) {
+		printf("---");
+	}
+	printf("\n");
+
+	uint samples_to_show = config->module.samples;
+	if (samples_to_show > 20) {
+		samples_to_show = 20;
+		printf("(Showing first 20 of %u samples)\n", config->module.samples);
+	}
+
+	for (uint s = 0; s < samples_to_show; s++) {
+		uint16_t v = config->dma.dma_buffer[s];
+		printf("%6u:", s);
+
+		/* Print bits for each pin in pin_map order (pin_map[0] -> first bit printed) */
+		for (uint i = 0; i < config->module.pin_count; i++) {
+			uint bit = (v >> i) & 0x1u;
+			printf("  %u", bit);
+		}
+
+		printf("  0x%04X\n", v);
+	}
+
+	if (config->module.samples > 20) {
+		printf("...\n");
+		uint16_t v = config->dma.dma_buffer[config->module.samples - 1];
+		printf("%6u:", config->module.samples - 1);
+		for (uint i = 0; i < config->module.pin_count; i++) {
+			uint bit = (v >> i) & 0x1u;
+			printf("  %u", bit);
+		}
+		printf("  0x%04X\n", v);
+	}
+}
+
+void ana_config_pio_test_pio_direct(struct ana_config_system *config)
+{
+	printf("\n=== Testing PIO without DMA ===\n");
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+	pio_sm_clear_fifos(config->pio.instance, config->pio.sm);
+	pio_sm_restart(config->pio.instance, config->pio.sm);
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, true);
+
+	sleep_ms(100);
+
+	uint fifo_level = pio_sm_get_rx_fifo_level(config->pio.instance, config->pio.sm);
+	printf("PIO FIFO level: %u\n", fifo_level);
+
+	if (fifo_level > 0) {
+		uint16_t data = pio_sm_get(config->pio.instance, config->pio.sm);
+		printf("Direct PIO read: 0x%04X\n", data);
+
+		for (uint i = 0; i < config->module.pin_count; i++) {
+			uint bit = (data >> i) & 0x1u;
+			printf("  Pin %u: %u\n", config->module.pin_base + i, bit);
+		}
+	} else {
+		printf("ERROR: No data in PIO FIFO!\n");
+		printf("Check: \n");
+		printf("  1. PIO program loaded correctly\n");
+		printf("  2. Pins configured as inputs\n");
+		printf("  3. Clock divider set correctly\n");
+	}
+
+	pio_sm_set_enabled(config->pio.instance, config->pio.sm, false);
+}
+
+uint16_t *ana_config_pio_get_buffer(struct ana_config_system *config)
+{
+	return config->dma.dma_buffer;
 }
